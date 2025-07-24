40686d97a9beff572e99a978ba588e3e9258a440
Gabriel Wanderley <113439146+Gabrielgsw@users.noreply.github.com>
2025-07-24 09:12:40 -0300

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -1,3 +1,9 @@
+// Atividade 2VA - Arquitetura e Organizacao de Computadores [2025.1]
+// Gabriel Germano dos Santos Wanderley
+// Samara Accioly
+// Vitor Barros de Carvalho
+// Wellington Viana da Silva Junior
+
 
 module data_memory (address, writeData, readData, memWrite, memRead);
 // Declarar parâmetros
