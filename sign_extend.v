ef636970d85e34d90eedf8a4d2ae1df176341566
Gabriel Wanderley <113439146+Gabrielgsw@users.noreply.github.com>
2025-07-24 09:13:15 -0300

    Update sign_extend.v

--- sign_extend.v
+++ sign_extend.v
@@ -1,3 +1,10 @@
+// Atividade 2VA - Arquitetura e Organizacao de Computadores [2025.1]
+// Gabriel Germano dos Santos Wanderley
+// Samara Accioly
+// Vitor Barros de Carvalho
+// Wellington Viana da Silva Junior
+
+
 module sign_extend(input wire[15:0] instruction, output wire[31:0] out); 
 
 	assign out = {{16{instruction[15]}}, instruction};
