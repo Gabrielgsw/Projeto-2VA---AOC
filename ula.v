27afad241defde97e30f0486d025927da4e7cc75
Gabriel Wanderley <113439146+Gabrielgsw@users.noreply.github.com>
2025-07-24 09:13:23 -0300

    Update ula.v

--- ula.v
+++ ula.v
@@ -1,3 +1,10 @@
+// Atividade 2VA - Arquitetura e Organizacao de Computadores [2025.1]
+// Gabriel Germano dos Santos Wanderley
+// Samara Accioly
+// Vitor Barros de Carvalho
+// Wellington Viana da Silva Junior
+
+
 module ula (in1, in2, op, result, zero_flag);
 
 // Portas
