c35d9e52e4fcdadad4c896c89bfadbecd9e12b4b
Vitor Barros <vbarros562@gmail.com>
2025-07-24 11:10:02 -0300

    Update sign_extend.v

--- sign_extend.v
+++ sign_extend.v
@@ -5,8 +5,8 @@
 // Wellington Viana da Silva Junior
 
 
-module sign_extend(input wire[15:0] instruction, output wire[31:0] out); 
-
+module sign_extend(input wire[15:0] instruction, output wire[31:0] out); // entrada de 16 bits e saida de 32 bits
+	// repete 16 vezes o bit ins[15] (mais significativo) e faz concatenacao com a instrucao total
 	assign out = {{16{instruction[15]}}, instruction};
 	
 endmodule
